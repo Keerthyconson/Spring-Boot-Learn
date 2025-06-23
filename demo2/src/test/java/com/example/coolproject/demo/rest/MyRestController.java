8d5b08699b51b4cbc1c99ed47f5b94c71054b61d
Keerthyconson <59124694+Keerthyconson@users.noreply.github.com>
2025-06-23 16:26:55 -0400

    Day 2

---
+++ demo2/src/test/java/com/example/coolproject/demo/rest/MyRestController.java
@@ -0,0 +1,12 @@
+package com.example.coolproject.demo.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestController {
+
+    @Test
+    void contextLoads() {
+    }
+}
